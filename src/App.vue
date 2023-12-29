02f3da7faf58b05228f91a5a992be7eb52168014
Furkan Bozok <118224028+FrknB@users.noreply.github.com>
2023-12-29 15:51:20 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,240 @@
+<template>
+    <div id="container">
+
+        <section class="header">
+            <h2>BLABLA</h2>
+            <h3>Restaurant</h3>
+        </section>
+
+        <section class="product-items">
+            <div class="items">
+                <a href="#hot-drink">Sıcak İçecekler</a>
+                <a href="#cold-drink">Soğuk İçecekler</a>
+                <a href="#sweet">Tatlılar</a>
+                <a href="#tosts">Tostlar</a>
+            </div>
+        </section>
+    
+        <section class="menu-list" id="hot-drink">
+            <h1>Sıcak İçecekler</h1>
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/kahve.png" alt="kahve">
+                    </div>
+                    <div class="details">
+                        <p>Türk Kahvesi</p> |
+                        <p>40 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/sicakcikolata.png" alt="sicakcikolata">
+                    </div>
+                    <div class="details">
+                        <p>Sıcak Çikolata</p> |
+                        <p>38 TL</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/tea.png" alt="tea">
+                    </div>
+                    <div class="details">
+                        <p>Çay</p> |
+                        <p>7 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/sutlukahve.png" alt="sutlukahve">
+                    </div>
+                    <div class="details">
+                        <p>Sütlü Kahve</p> |
+                        <p>33 TL</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="menu-list" id="cold-drink">
+            <h1>Soğuk İçecekler</h1>
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/caprisun.png" alt="caprisun">
+                    </div>
+                    <div class="details">
+                        <p>Caprisun</p> |
+                        <p>12 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/sprite.png" alt="sprite">
+                    </div>
+                    <div class="details">
+                        <p>Sprite</p> |
+                        <p>12 TL</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/limonata.png" alt="limonata">
+                    </div>
+                    <div class="details">
+                        <p>Limonata</p> |
+                        <p>20 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/drinks/alkolüz.png" alt="alkolüz">
+                    </div>
+                    <div class="details">
+                        <p>Alkolsüz İçecek</p> |
+                        <p>25 TL</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="menu-list" id="sweet">
+            <h1>Tatlılar</h1>
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/sweets/baklava.png" alt="baklava">
+                    </div>
+                    <div class="details">
+                        <p>Baklava</p> |
+                        <p>80 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/sweets/cheesecake.png" alt="cheesecake">
+                    </div>
+                    <div class="details">
+                        <p>Cheesecake</p> |
+                        <p>65 TL</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/sweets/cupcake.png" alt="cupcake">
+                    </div>
+                    <div class="details">
+                        <p>CupCake</p> |
+                        <p>50 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/sweets/kunefe.png" alt="kunefe">
+                    </div>
+                    <div class="details">
+                        <p>Künefe</p> |
+                        <p>60 TL</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="menu-list" id="tosts">
+            <h1>Tostlar</h1>
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/tost/karisiktost.png" alt="karisiktost">
+                    </div>
+                    <div class="details">
+                        <p>Karışık tost</p> |
+                        <p>35 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/tost/kasarlitost.png" alt="kasarlitost">
+                    </div>
+                    <div class="details">
+                        <p>Kaşarlı Tost</p> |
+                        <p>28 TL</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/tost/pizzatost.png" alt="pizzatost">
+                    </div>
+                    <div class="details">
+                        <p>Pizza Tost</p> |
+                        <p>50 TL</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="list-items">
+                <div class="item">
+                    <div class="image">
+                        <img src="../src/assets/img/tost/sucuklutost.png" alt="sucuklutost">
+                    </div>
+                    <div class="details">
+                        <p>Suculu Tost</p> |
+                        <p>40 TL</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
